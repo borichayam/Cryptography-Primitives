f393fffe9fa9b25e440a41be4b3356ff24670b94
borichayam <shs6817@duksung.ac.kr>
2026-09-18 12:38:11 +0900

    Add powm example

---
+++ powm.cpp
@@ -0,0 +1,35 @@
+int powm(int base, int exp, int mod) {
+    int r = 1;
+    int i;
+
+    for (i = 0; i < exp; i++) {
+        r *= base;
+        r %= mod;
+    }
+
+    return r;
+}
+
+int main(void) {
+    int base;
+    int exp;
+    int mod;
+    int result;
+
+    printf("base: ");
+    scanf("%d", &base);
+
+    printf("exp: ");
+    scanf("%d", &exp);
+
+    printf("mod: ");
+    scanf("%d", &mod);
+
+    result = powm(base, exp, mod);
+
+    printf("result: %d\n", result);
+
+    // printf("%d\n", powm(3, 100, 23));
+
+    return 0;
+}
